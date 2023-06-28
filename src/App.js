33ae7a02ce6c4667d289ad781ca045be92635e80
Pavlo Grebennikov <pavlogrebennikov@MacBook-Air-Pavlo.local>
2023-06-28 21:37:33 +0200

    changes

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Footer from "./Components/Footer";
 
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <div className="App">
         <div className="container--app">
           <div className="background"></div>
